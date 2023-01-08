6446f02408065b8c1a7ceeb6eaaa06d20ca56b25
Hououin Kyouma <inwazzin@gmail.com>
2023-01-08 17:26:56 +0100

    select pointer

--- Shared/Extensions/NavigationManagerExtensions.cs
+++ Shared/Extensions/NavigationManagerExtensions.cs
@@ -5,6 +5,6 @@ namespace pw_2022.Shared.Extensions;
 public static class NavigationManagerExtensions {
   public static IEnumerable<string> PathSegments(this NavigationManager manager) =>
     manager.ToBaseRelativePath(manager.Uri).Split('/', StringSplitOptions.RemoveEmptyEntries);
-    public static bool IsLastSegment(this NavigationManager manager, string segment) =>
-      manager.PathSegments().LastOrDefault() == segment;
+  public static bool IsLastSegment(this NavigationManager manager, string segment) =>
+    manager.PathSegments().LastOrDefault() == segment;
 }
