feda71d2050ec4f0093d91b328c5f54d39bb55fb
Hououin Kyouma <inwazzin@gmail.com>
2023-01-08 19:25:03 +0100

    interactive click

---
+++ UI/Shared/Extensions/ObjectExtensions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace UI.Shared.Extensions;
+
+public static class ObjectExtensions {
+  public static T? Access<T>(this object @object, string param) where T : class =>
+    @object.GetType().GetProperty(param)?.GetValue(@object) as T;
+}
