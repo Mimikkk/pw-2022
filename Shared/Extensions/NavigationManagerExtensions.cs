614ac3b8c7bf258c7036b982dfb40f552bc6cff1
Hououin Kyouma <inwazzin@gmail.com>
2023-01-08 01:48:09 +0100

    home on empty breadcrumb

--- Shared/Extensions/NavigationManagerExtensions.cs
+++ Shared/Extensions/NavigationManagerExtensions.cs
@@ -6,5 +6,5 @@ public static class NavigationManagerExtensions {
   public static IEnumerable<string> PathSegments(this NavigationManager manager) =>
     manager.ToBaseRelativePath(manager.Uri).Split('/', StringSplitOptions.RemoveEmptyEntries);
     public static bool IsLastSegment(this NavigationManager manager, string segment) =>
-      manager.PathSegments().Last() == segment;
+      manager.PathSegments().LastOrDefault() == segment;
 }
